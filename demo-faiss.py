50eed211dc6a154b788d81e69ce23be8e1f83a82
AlfonsoParraga <32110784+AlfonsoParraga@users.noreply.github.com>
2024-03-16 19:54:58 +0100

    Demo uso Faiss

---
+++ demo-faiss.py
@@ -0,0 +1,31 @@
+import numpy as np
+import faiss
+filename = 'test2dpoints.index'
+load_file = True
+
+query = np.expand_dims(np.array([-1,-1]), axis=0)
+corpus = np.array([[0,0],
+                    [1,2],
+                    [2,1],
+                    [-1,0],
+                    [1,0],
+                    [1,1],
+                    [-1,-1]
+                    ])
+
+if not load_file:
+    # Creamos faiss
+    faiss_db = faiss.IndexFlatL2(corpus.shape[1])
+
+    # Añadimos "embeddings" a faiss 
+    faiss_db.add(corpus)
+
+    # Guarda en disco el indice
+    faiss.write_index(faiss_db, filename)
+else:
+    # Creamos faiss desde fichero
+    faiss_db = faiss.read_index(filename)
+
+D, I = faiss_db.search(query, k=3)
+
+print("D", D, "\nI", I)
