5cb388cc8cca885373851e6a6824cc34f40a5ffc
CarloHSUA <chspairani@gmail.com>
2024-03-17 19:25:34 +0100

    UPDATE: read csv

--- model/sentence_model.py
+++ model/sentence_model.py
@@ -1,7 +1,10 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
+import spacy  # Reconocimiento de entidades
 
+# Instalamos el modelo en inglés de SpaCy
+spacy.cli.download('en_core_web_sm')
 
 class SencenceModel():
     def __init__(self, device) -> None:
@@ -9,6 +12,9 @@ class SencenceModel():
         self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-mpnet-base-v2')
         self.model = AutoModel.from_pretrained('sentence-transformers/all-mpnet-base-v2').to(device)
         self.device = device
+        spacy.cli.download('en_core_web_sm')
+        self.nlp = spacy.load('en_core_web_sm')
+
 
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0] #First element of model_output contains all token embeddings
@@ -16,8 +22,9 @@ class SencenceModel():
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def calculate_embedding(self, sentences):
+        
         encoded_input = self.tokenizer(sentences, padding=True, truncation=True, return_tensors='pt').to(self.device)
-        print(encoded_input)
+        
         self.model.eval()
         # Compute token embeddings
         with torch.no_grad():
@@ -28,4 +35,4 @@ class SencenceModel():
 
         # Normalize embeddings
         return F.normalize(sentence_embeddings, p=2, dim=1)
-    
+
