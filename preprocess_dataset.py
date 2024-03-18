f1cd9e48af3a0730b7f15d946a2384e4e6067f3e
AlfonsoParraga <32110784+AlfonsoParraga@users.noreply.github.com>
2024-03-18 18:06:20 +0100

    Preprocess dataset with embeddings

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,40 +1,129 @@
 import numpy as np
+import os
 import pandas as pd
 import spacy
+from tqdm import tqdm
 from datasets import load_dataset
+from model.sentence_model import SencenceModel
+import torch
+from faiss_wrapper import FaissWrapper
 
-hf_dataset = load_dataset("glnmario/news-qa-summarization", split='train')
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+print("DEVICE: ", DEVICE)
 
+class Dataset:
+    def __init__(self, reload = True, save_datasets = True, verbose = 0, generate_embeddings = True) -> None:
+        self.reload = reload
+        self.save_datasets = save_datasets
+        self.verbose = verbose
+        self.corpus_filename = 'source_dataset.csv'
+        self.sentences_filename = 'sentence_dataset.csv'
+        self.corpus_dataset = None
+        self.sentence_dataset = None
+        self.get_corpus_dataset()
+        self.get_sentences_dataset(generate_embeddings)
 
-corpus_dataset = pd.DataFrame.from_dict(hf_dataset)
-# Creates column index
-corpus_dataset['index'] = np.arange(corpus_dataset.shape[0])
+    def load_corpus(self, columns):
+        hf_dataset = load_dataset("glnmario/news-qa-summarization", split='train')
+        if self.verbose > 0:
+            print("Downloaded dataset")
+        self.corpus_dataset = pd.DataFrame.from_dict(hf_dataset)
 
-# Reorder columns
-corpus_dataset = corpus_dataset[['index', 'story', 'questions', 'answers', 'summary']]
-# Saves the dataset from Hugging Face
-corpus_dataset.to_csv('source_dataset.csv')
+        # Creates column index
+        self.corpus_dataset['index'] = np.arange(self.corpus_dataset.shape[0])
+        # Reorder columns
+        self.corpus_dataset = self.corpus_dataset[columns]
 
-# We download English model for Spacy
-spacy.cli.download('en_core_web_sm')
+    def get_corpus_dataset(self):
+        # Loads corpus dataset
+        if self.reload or not os.path.isfile(self.corpus_filename):
+            self.load_corpus(['index', 'story'])
+            if self.verbose > 0:
+                    print("Loaded documents from Hugging Face")
+            if self.save_datasets:
+                self.save_corpus_to_disk()
+                if self.verbose > 0:
+                    print("Saved documents to disk")
+        else:
+            self.load_corpus_from_disk()
+            print(f"Loaded documents from {self.corpus_filename}")
 
-# Loads the English model
-nlp = spacy.load('en_core_web_sm')
+    def get_sentences_dataset(self, generate_embeddings = True):
+        # Loads sentence dataset
+        if self.reload or not os.path.isfile(self.sentences_filename):
+            self.process_sentences(generate_embeddings = generate_embeddings)
+            if self.save_datasets:
+                self.save_sentences_to_disk()
+                if self.verbose > 0:
+                    print("Saved sentences to disk")
+        else:
+            self.load_sentences_from_disk()
+            print(f"Loaded documents from {self.sentences_filename}")
+        
+    def load_corpus_from_disk(self):
+        self.corpus_dataset = pd.read_csv(self.corpus_filename)
 
-sentence_list = []
+    def save_corpus_to_disk(self):
+        self.corpus_dataset.to_csv(self.corpus_filename)
 
-def split_into_sentences(row):
-    document = nlp(row['story'])
-    for sentence in list(document.sents):
-        new_row = {}
-        new_row['doc_index'] = row['index']
-        new_row['index'] = len(sentence_list) + 1
-        new_row['sentence'] = sentence
-        new_row['embeddings'] = '' # TODO
-        sentence_list.append(new_row)
+    def load_sentences_from_disk(self):
+        self.sentence_dataset = pd.read_csv(self.sentences_filename)
 
-corpus_dataset.apply(split_into_sentences, axis=1)
+    def save_sentences_to_disk(self):
+        self.sentence_dataset[['doc_index', 'index', 'sentence']].to_csv(self.sentences_filename)
 
+    def process_sentences(self, generate_embeddings = True):
+        if not spacy.util.is_package("en_core_web_sm"):
+            # We download English model for Spacy
+            spacy.cli.download('en_core_web_sm')
+        nlp = spacy.load('en_core_web_sm')
 
-sentence_dataset = pd.DataFrame(sentence_list, columns=['doc_index', 'index', 'sentence', 'embeddings'])
-sentence_dataset.to_csv('sentence_dataset.csv')
+        sentence_list = []
+        with tqdm(total=len(self.corpus_dataset), desc="Processing documents to sentences") as pbar:
+            if generate_embeddings:
+                model = SencenceModel(device = DEVICE)
+
+            def split_into_sentences(row):
+                document = nlp(row['story'])
+                if generate_embeddings:
+                    embeddings = model.calculate_embedding(list(map(lambda e: str(e), document.sents)))
+
+                for sentence_idx, sentence in enumerate(list(document.sents)):
+                    new_row = {}
+                    new_row['doc_index'] = row['index']
+                    new_row['sentence'] = sentence
+                    new_row['embeddings'] = None if not generate_embeddings else embeddings[sentence_idx]
+                    sentence_list.append(new_row)
+                pbar.update(1)
+
+            self.corpus_dataset.apply(split_into_sentences, axis=1)
+
+        pbar.close()
+
+        self.sentence_dataset = pd.DataFrame(sentence_list, columns=['doc_index', 'index', 'sentence', 'embeddings'])
+        self.sentence_dataset['index'] = np.arange(self.sentence_dataset.shape[0])
+
+    def generate_embeddings(self):
+        if self.sentence_dataset['embeddings'].isnull().any(axis=0):
+            model = SencenceModel(device = DEVICE)
+
+            with tqdm(total=len(self.sentence_dataset), desc="Calculating embeddings") as pbar:
+                def calculate_embedding(row):
+                    embeddings = model.calculate_embedding(list(row['sentence']))
+                    row['embeddings'] = embeddings[0].cpu()
+                    pbar.update(1)
+
+                self.sentence_dataset.apply(calculate_embedding, axis=1)
+
+            pbar.close()
+
+if __name__ == "__main__":
+    dataset = Dataset(reload = False, save_datasets = False, verbose = 1, generate_embeddings = True)
+    index = FaissWrapper("cnn_news.index")
+    if index.faiss_db is None:
+        dataset.generate_embeddings()
+        index.add_embeddings_with_ids(dataset.sentence_dataset['embeddings'].to_numpy(), dataset.sentence_dataset['index'].to_numpy())
+        index.save_to_disk()
+
+    while True:
+        pass
